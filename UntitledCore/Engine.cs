52df8cd0e137ff7954df3e971861069ee898be96
Tatsu011 <cmcleod18@hotmail.com>
2021-01-15 15:43:29 -0800

    Add project files.

---
+++ UntitledCore/Engine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UntitledCore
+{
+    public class Engine
+    {
+        public Engine()
+        {
+            //Initialize
+        }
+
+        public void Run()
+        {
+            //Run the game. (Update/Draw)
+        }
+
+         ~Engine()
+        {
+            //cleanup
+        }
+
+    }
+}
---
+++ UntitledGameEngine/Program.cs
@@ -0,0 +1,49 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+
+namespace UntitledGameEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Raylib.InitWindow(800, 480, "Hello World");
+            Raylib.SetTargetFPS(60);
+            List<string> log = new List<string>();
+
+
+            int f = 0;
+            int seconds = 0;
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                Raylib.DrawText("Hello, world!", 300, 12, 20, Color.BLACK);
+
+                log.Reverse();
+                int YPos = 460;
+                foreach(string s in log)
+                {
+                    Raylib.DrawText(s, 12, YPos, 20, Color.BLACK);
+                    YPos -= 20;
+                }
+                log.Reverse();
+
+                Raylib.EndDrawing();
+                f++;
+                if(f >= 60)
+                {
+                    f = 0;
+                    seconds++;
+                    log.Add($"{seconds}...");
+                }
+                
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
