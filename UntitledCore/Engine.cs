d8aab67ce68d0052549aea918bf2bf0ac9e27480
Tatsu011 <cmcleod18@hotmail.com>
2021-01-15 16:30:47 -0800

    Removed unused project Moved engine functions to Engine Core

--- UntitledCore/Engine.cs
+++ UntitledCore/Engine.cs
@@ -1,17 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
+using Raylib_cs;
 
 namespace UntitledCore
 {
     public class Engine
     {
+        List<string> Log;
         public Engine()
         {
             //Initialize
+            Raylib.InitWindow(800, 480, "Hello World");
+            Raylib.SetTargetFPS(60);
+            Log = new List<string>();
         }
 
         public void Run()
         {
             //Run the game. (Update/Draw)
+            int f = 0;
+            int seconds = 0;
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                Raylib.DrawText("Hello, world!", 300, 12, 20, Color.BLACK);
+
+                Log.Reverse();
+                int YPos = 460;
+                foreach (string s in Log)
+                {
+                    Raylib.DrawText(s, 12, YPos, 20, Color.BLACK);
+                    YPos -= 20;
+                }
+                Log.Reverse();
+
+                Raylib.EndDrawing();
+                f++;
+                if (f >= 60)
+                {
+                    f = 0;
+                    seconds++;
+                    Log.Add($"{seconds}...");
+                }
+
+            }
+
+            Raylib.CloseWindow();
         }
 
          ~Engine()
--- UntitledGameEngine/Program.cs
+++ UntitledGameEngine/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Raylib_cs;
 using System;
 using System.Collections.Generic;
+using UntitledCore;
 
 namespace UntitledGameEngine
 {
@@ -9,41 +10,13 @@ namespace UntitledGameEngine
         static void Main(string[] args)
         {
 
-            Raylib.InitWindow(800, 480, "Hello World");
-            Raylib.SetTargetFPS(60);
-            List<string> log = new List<string>();
+            Engine engine = new Engine();
 
 
-            int f = 0;
-            int seconds = 0;
-            while (!Raylib.WindowShouldClose())
-            {
-                Raylib.BeginDrawing();
-                Raylib.ClearBackground(Color.WHITE);
+            engine.Run();
 
-                Raylib.DrawText("Hello, world!", 300, 12, 20, Color.BLACK);
 
-                log.Reverse();
-                int YPos = 460;
-                foreach(string s in log)
-                {
-                    Raylib.DrawText(s, 12, YPos, 20, Color.BLACK);
-                    YPos -= 20;
-                }
-                log.Reverse();
-
-                Raylib.EndDrawing();
-                f++;
-                if(f >= 60)
-                {
-                    f = 0;
-                    seconds++;
-                    log.Add($"{seconds}...");
-                }
-                
-            }
-
-            Raylib.CloseWindow();
+            
         }
     }
 }
